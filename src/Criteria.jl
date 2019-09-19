bee8ebdbec64647728e9b9202717ce0661fafd94
Regan Baucke <regan02@gmail.com>
2019-09-19 15:36:28 +0200

    added criteria struct

---
+++ src/Criteria.jl
@@ -0,0 +1,28 @@
+####
+# This file defines the criteria type and related functions
+####
+
+struct Criteria
+	# tolerances
+	abstol::Real
+	reltol::Real
+
+	# time
+	time::Real
+
+	#iterations
+	iterations::Int64
+
+	# number of calls to the oracles
+	m_calls::Int64
+	c_calls::Int64
+end
+
+function Criteria(;abstol::Float64=0.0,
+	 			   reltol::Float64=0.0,
+				   time::Float64=Inf,
+				   iterations::Int64=9223372036854775807,
+				   m_calls::Int64=9223372036854775807,
+				   c_calls::Int64=9223372036854775807)
+	 return Criteria(abstol,reltol,time,iterations,m_calls,c_calls)
+end
--- src/Sprocket.jl
+++ src/Sprocket.jl
@@ -2,4 +2,5 @@ module Sprocket
 	include("./Point.jl")
 	include("./Cut.jl")
 	include("./Bound.jl")
+	include("./Criteria.jl")
 end
