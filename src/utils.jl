b2f8a923de225bb4b22219933eddc03fab29e448
Regan Baucke <regan02@gmail.com>
2019-10-05 15:16:12 +0200

    Added helper function

--- src/utils.jl
+++ src/utils.jl
@@ -1,10 +1,15 @@
 ############
 # Utility functions and miscillaneous helpers
 ############
+using JuMP
 
 #### delete whole array of variables from a jump model
 function JuMP.delete(m::JuMP.Model,vars::AbstractArray)
     for var in vars
         JuMP.delete(m,var)
     end
+end
+
+function JuMP.size(::JuMP.VariableRef)
+    return ()
 end
