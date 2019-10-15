7c548159abf95661204e9d1752c55dd29bc7c065
Regan Baucke <regan02@gmail.com>
2019-10-15 14:30:58 +0200

    Added a two dimensional test

--- test/baucke.jl
+++ test/baucke.jl
@@ -3,54 +3,32 @@ include("../src/baucke.jl")
 # using Sprocket
 using JuMP
 
+##
+# One dimensional problem, no control
+##
+
 function build_problem()
 	#create an empty problem
 	problem = Sprocket.Problem()
 
 	# add one real valued random variable to the problem
-	xi = Sprocket.Variable(name=:xi, size=(),type = Sprocket.Random())
-	# another = Sprocket.Variable(:flat,(),Sprocket.Random())
+	xi = Sprocket.Variable(name=:xi, size=(),type=Sprocket.Random())
 	Sprocket.add_variable(problem,xi)
-	# Sprocket.add_variable(problem,another)
-
-	# @variable(problem.model, xi)
-    # delete(problem.model,xi)
-    ###
 
 	# objective function is defined through the cutting plane oracle
 	function my_cutting_plane_oracle()
-		function zero_order(vars)
-			return vars[:xi]^4
-			# return 0
-		end
-		function first_order(vars)
+		function oracle(vars)
+			value = vars[:xi]^4
 			grad = deepcopy(vars)
 			grad[:xi] = 4*vars[:xi]^3
-			return grad
+			return (value,grad)
 		end
-		return (zero_order,first_order)
+		return oracle
 	end
 
 	# distribution of random variables is defined through the measure oracle
 	function my_measure_oracle()
-		function zero_order(vars_1,vars_2)
-			left = vars_1[:xi]
-			right = vars_2[:xi]
-			if vars_1[:xi] <= 0
-				left = 0
-			end
-			if vars_1[:xi] >= 1
-				left = 1
-			end
-			if vars_2[:xi] <= 0
-				right = 0
-			end
-			if vars_2[:xi] >= 1
-				right = 1
-			end
-			return right-left
-		end
-		function first_order(vars_1,vars_2)
+		function oracle(vars_1,vars_2)
 			left = vars_1[:xi]
 			right = vars_2[:xi]
 			if vars_1[:xi] <= 0
@@ -67,9 +45,99 @@ function build_problem()
 			end
 			fresh = deepcopy(vars_1)
 			fresh[:xi] = (right^2 - left^2)/2
-			return fresh
+			return (right-left,fresh)
 		end
-		return (zero_order,first_order)
+		return oracle
+	end
+
+    problem.c_oracle = my_cutting_plane_oracle()
+    problem.m_oracle = my_measure_oracle()
+
+	return problem
+end
+
+# my_prob = build_problem()
+# Sprocket.Criteria(reltol=0.1)
+# states = Sprocket.solve(BauckeAlgorithm(),my_prob,Sprocket.Criteria(reltol=0.1))
+
+
+
+##
+# Test 2, two dimensional problem no control
+##
+
+function build_problem()
+	#create an empty problem
+	problem = Sprocket.Problem()
+
+	# add one real valued random variable to the problem
+	xi = Sprocket.Variable(name=:xi, size=(),type = Sprocket.Random())
+	Sprocket.add_variable(problem,xi)
+
+	# add another
+	lam = Sprocket.Variable(name=:lam, size=(),type = Sprocket.Random())
+	Sprocket.add_variable(problem,lam)
+
+	# objective function is defined through the cutting plane oracle
+	function my_cutting_plane_oracle()
+		function oracle(vars)
+			value = vars[:xi]^2 + 3*vars[:lam]^2 + 0.1*vars[:lam]*vars[:xi]
+
+			grad = deepcopy(vars)
+			grad[:xi] = 2*vars[:xi] + 0.1*vars[:lam]
+			grad[:lam] = 0.1*vars[:xi] + 6*vars[:lam]
+
+			# value = vars[:xi]^4 + vars[:lam]^2
+			#
+			# grad = deepcopy(vars)
+			# grad[:xi] = 4*vars[:xi]^3
+			# grad[:lam] = 2*vars[:lam]
+			return (value,grad)
+		end
+		return oracle
+	end
+
+	# distribution of random variables is defined through the measure oracle
+	function my_measure_oracle()
+		function oracle(vars_1,vars_2)
+			left_xi = vars_1[:xi]
+			right_xi = vars_2[:xi]
+			if vars_1[:xi] <= 0
+				left_xi = 0
+			end
+			if vars_1[:xi] >= 1
+				left_xi = 1
+			end
+			if vars_2[:xi] <= 0
+				right_xi = 0
+			end
+			if vars_2[:xi] >= 1
+				right_xi = 1
+			end
+			factor_xi = right_xi - left_xi
+
+			left_lam = vars_1[:lam]
+			right_lam = vars_2[:lam]
+			if vars_1[:lam] <= 0
+				left_lam = 0
+			end
+			if vars_1[:lam] >= 1
+				left_lam = 1
+			end
+			if vars_2[:lam] <= 0
+				right_lam = 0
+			end
+			if vars_2[:lam] >= 1
+				right_lam = 1
+			end
+			factor_lam = right_lam - left_lam
+
+			fresh = deepcopy(vars_1)
+			fresh[:xi] = (right_xi^2 - left_xi^2)*factor_lam/2
+			fresh[:lam] = (right_lam^2 - left_lam^2)*factor_xi/2
+			return (factor_lam*factor_xi,fresh)
+		end
+		return oracle
 	end
 
     problem.c_oracle = my_cutting_plane_oracle()
@@ -79,7 +147,7 @@ function build_problem()
 end
 
 my_prob = build_problem()
-
 # Sprocket.Criteria(reltol=0.1)
-
 states = Sprocket.solve(BauckeAlgorithm(),my_prob,Sprocket.Criteria(reltol=0.1))
+
+# ANSWER IS 0.53333333333
