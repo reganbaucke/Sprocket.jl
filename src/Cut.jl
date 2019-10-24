5896002134c424b3cc57df9075da21d761c80bf8
Regan Baucke <regan02@gmail.com>
2019-10-24 13:24:10 +0200

    added cut projection

--- src/Cut.jl
+++ src/Cut.jl
@@ -15,3 +15,19 @@ function generate_cut(c_oracle,point::Point)
 	(val,grad) = c_oracle(point)
 	return Cut(point,val,grad)
 end
+
+function apply_cut_at(cut::Cut, point::Point)
+	new_point = sub_point(cut.point,setdiff(get_vars(cut.point),get_vars(point)))
+	new_value = cut.value + inner(point-sub_point(cut.point,get_vars(point)), sub_point(cut.grad,get_vars(point)))
+	new_grad = sub_point(cut.grad,setdiff(get_vars(cut.grad),get_vars(point)))
+	Cut(new_point,new_value,new_grad)
+end
+
+
+
+###
+# Small suite of tests
+###
+function test()
+
+end
