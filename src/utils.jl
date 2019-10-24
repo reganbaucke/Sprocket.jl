69e83f4f464a100f4660cdd5f0baa3f330efeea2
Regan Baucke <regan02@gmail.com>
2019-10-24 13:19:35 +0200

    Added definition of Option type

--- src/utils.jl
+++ src/utils.jl
@@ -13,3 +13,43 @@ end
 function JuMP.size(::JuMP.VariableRef)
     return ()
 end
+
+
+# Option values for error handling in a functional way
+struct Some{T}
+    value::T
+end
+
+const Option{T} = Union{Some{T},Nothing}
+
+function get(thing:: Some{T}) where T
+    thing.value
+end
+
+function Base.map(f,thing::Some{T}) where T
+    thing |> get |> f |> Some
+end
+
+function Base.map(f,thing::Nothing)
+    nothing
+end
+
+function flat_map(f,thing::Some{T}) where T
+    thing |> get |> f
+end
+
+function flat_map(f,thing::Nothing) where T
+    nothing
+end
+
+function some_function(x::Number)
+    if x <= 5
+        Some(x+6)
+    else
+        nothing
+    end
+end
+
+function >>(thing::Option,f)
+    flat_map(f,thing)
+end
